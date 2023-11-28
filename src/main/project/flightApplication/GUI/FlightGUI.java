05ab0658d9793508c8ca9934a0b823faa83404f2
MohamedEbdalla <Mohamed.ebdalla@ucalgary.ca>
2023-11-28 14:31:37 -0700

    previous and next buttons working

--- src/main/project/flightApplication/GUI/FlightGUI.java
+++ src/main/project/flightApplication/GUI/FlightGUI.java
@@ -1,5 +1,6 @@
 package main.project.flightApplication.GUI;
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,6 +11,9 @@ import main.project.flightApplication.Admin;
 public class FlightGUI extends JFrame {
     private Admin admin = new Admin();
     private int currentIndex = 0;
+    private JPanel mainPanel;
+    private JButton prevButton;
+    private JButton nextButton;
 
     public JPanel createFlightDetailsPanel(Flight flight) {
         JPanel detailsPanel = new JPanel(new GridLayout(5, 1));
@@ -31,7 +35,11 @@ public class FlightGUI extends JFrame {
     }
 
     public void displayFlight(Flight flight) {
-        JPanel mainPanel = new JPanel();
+        if (mainPanel != null) {
+            remove(mainPanel);
+        }
+
+        mainPanel = new JPanel(); // Initialize mainPanel
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
 
         JPanel imagePanel = new JPanel();
@@ -41,17 +49,11 @@ public class FlightGUI extends JFrame {
         JPanel detailsPanel = new JPanel();
         detailsPanel.setLayout(new BoxLayout(detailsPanel, BoxLayout.Y_AXIS));
 
-        JPanel firstFlightPanel = new JPanel();
-        firstFlightPanel.add(createFlightDetailsPanel(flight));
+        // Calculate the endIndex based on currentIndex and flights size
+        int endIndex = Math.min(currentIndex + 2, admin.getAllFlights().size());
 
-        detailsPanel.add(firstFlightPanel);
-
-        // Retrieve the second flight from the list (assuming there are at least two flights)
-        ArrayList<Flight> flights = admin.getAllFlights();
-        if (flights.size() > 1) {
-            JPanel secondFlightPanel = new JPanel();
-            secondFlightPanel.add(createFlightDetailsPanel(flights.get(1))); // Displaying the second flight
-            detailsPanel.add(secondFlightPanel);
+        for (int i = currentIndex; i < endIndex; i++) {
+            detailsPanel.add(createFlightDetailsPanel(admin.getAllFlights().get(i)));
         }
 
         mainPanel.add(imagePanel);
@@ -60,26 +62,42 @@ public class FlightGUI extends JFrame {
         add(mainPanel);
         pack();
         setVisible(true);
+        updatePrevButtonVisibility();
+        updateNextButtonVisibility();
     }
 
- 
+
+
     public FlightGUI() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JButton nextButton = new JButton("Next");
+        nextButton = new JButton("Next Page");
         nextButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 currentIndex += 2;
-                
+                displayFlight(null); // Pass null as argument to trigger redrawing of the panel
+
+            }
+        });
+        prevButton = new JButton("Previous Page");
+        prevButton.setVisible(false); // Initially set it invisible
+        prevButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                currentIndex = Math.max(0, currentIndex - 2); // Ensure currentIndex doesn't go below 0
+                displayFlight(null); // Pass null as argument to trigger redrawing of the panel
+                updatePrevButtonVisibility(); // Update the visibility of the previous button after moving to the previous page
             }
         });
 
         JPanel buttonPanel = new JPanel();
+        buttonPanel.add(prevButton);
         buttonPanel.add(nextButton);
 
         add(buttonPanel, BorderLayout.SOUTH);
-
+        displayFlight(null); // Initial display with currentIndex = 0
+        updatePrevButtonVisibility(); // Update the visibility initially
     }
+    
 
     private JLabel createImageLabel(String imagePath) {
         ImageIcon imageIcon = new ImageIcon(imagePath);
@@ -88,13 +106,32 @@ public class FlightGUI extends JFrame {
         return new JLabel(scaledIcon);
     }
 
-    public static void main(String[] args) {
-        FlightGUI display = new FlightGUI();
-        Admin admin = new Admin();
-        ArrayList<Flight> flights = admin.getAllFlights();
-
-        if (!flights.isEmpty()) {
-            display.displayFlight(flights.get(0));
+    private void updatePrevButtonVisibility() {
+        if (currentIndex > 0) {
+            prevButton.setVisible(true); // Set previous button visible if it's beyond the initial page and not at the end
+            System.out.print("prec");
+        } else {
+            prevButton.setVisible(false); // Hide previous button
         }
-}
+    }
+
+    private void updateNextButtonVisibility() {
+        int endIndex = Math.min(currentIndex + 2, admin.getAllFlights().size());
+        if (endIndex >= admin.getAllFlights().size()) {
+            nextButton.setVisible(false); // Hide next button on the last page
+        } else {
+            nextButton.setVisible(true); // Show next button if there are more pages
+        }
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            FlightGUI display = new FlightGUI();
+            Admin admin = new Admin();
+            ArrayList<Flight> flights = admin.getAllFlights();
+
+            if (!flights.isEmpty()) {
+                display.displayFlight(flights.get(0));
+            }
+        });
+    }
 }
