cf25627f5e9da760b6661f45d35a84fc82960c71
MohamedEbdalla <Mohamed.ebdalla@ucalgary.ca>
2023-11-28 13:18:32 -0700

    added next button, two flights currently displayed

--- src/main/project/flightApplication/GUI/FlightGUI.java
+++ src/main/project/flightApplication/GUI/FlightGUI.java
@@ -1,26 +1,17 @@
 package main.project.flightApplication.GUI;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import main.project.flightApplication.Flight;
 import main.project.flightApplication.Admin;
 
 public class FlightGUI extends JFrame {
-
     private Admin admin = new Admin();
+    private int currentIndex = 0;
 
-    public void displayFlight(Flight flight) {
-        JPanel mainPanel = new JPanel(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(10, 20, 10, 20);
-
-        // Panel for image
-        JPanel imagePanel = new JPanel();
-        JLabel imageLabel = createImageLabel("flightPic.jpg"); // Change this path to your image
-        imagePanel.add(imageLabel);
-
-        // Panel for flight details (right side)
+    public JPanel createFlightDetailsPanel(Flight flight) {
         JPanel detailsPanel = new JPanel(new GridLayout(5, 1));
         detailsPanel.setBorder(BorderFactory.createTitledBorder("Flight Details"));
 
@@ -28,7 +19,7 @@ public class FlightGUI extends JFrame {
         JLabel originLabel = new JLabel("Origin: " + flight.getOrigin());
         JLabel destinationLabel = new JLabel("Destination: " + flight.getDestination());
         JLabel departureLabel = new JLabel("Departure: " + flight.getDepartureDateTime());
-        JLabel arrivalLabel = new JLabel("Arrival: " + flight.getArrivalDateTime());
+        JLabel arrivalLabel = new JLabel("Arrival:      " + flight.getArrivalDateTime());
 
         detailsPanel.add(flightNumberLabel);
         detailsPanel.add(originLabel);
@@ -36,16 +27,58 @@ public class FlightGUI extends JFrame {
         detailsPanel.add(departureLabel);
         detailsPanel.add(arrivalLabel);
 
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        mainPanel.add(imagePanel, gbc);
+        return detailsPanel;
+    }
 
-        gbc.gridx = 1;
-        mainPanel.add(detailsPanel, gbc);
+    public void displayFlight(Flight flight) {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+
+        JPanel imagePanel = new JPanel();
+        JLabel imageLabel = createImageLabel("flightPic.jpg");
+        imagePanel.add(imageLabel);
+
+        JPanel detailsPanel = new JPanel();
+        detailsPanel.setLayout(new BoxLayout(detailsPanel, BoxLayout.Y_AXIS));
+
+        JPanel firstFlightPanel = new JPanel();
+        firstFlightPanel.add(createFlightDetailsPanel(flight));
+
+        detailsPanel.add(firstFlightPanel);
+
+        // Retrieve the second flight from the list (assuming there are at least two flights)
+        ArrayList<Flight> flights = admin.getAllFlights();
+        if (flights.size() > 1) {
+            JPanel secondFlightPanel = new JPanel();
+            secondFlightPanel.add(createFlightDetailsPanel(flights.get(1))); // Displaying the second flight
+            detailsPanel.add(secondFlightPanel);
+        }
+
+        mainPanel.add(imagePanel);
+        mainPanel.add(detailsPanel);
 
         add(mainPanel);
         pack();
         setVisible(true);
+    }
+
+ 
+    public FlightGUI() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton nextButton = new JButton("Next");
+        nextButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                currentIndex += 2;
+                
+            }
+        });
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(nextButton);
+
+        add(buttonPanel, BorderLayout.SOUTH);
+
     }
 
     private JLabel createImageLabel(String imagePath) {
@@ -63,6 +96,5 @@ public class FlightGUI extends JFrame {
         if (!flights.isEmpty()) {
             display.displayFlight(flights.get(0));
         }
-        //...
-    }
+}
 }
---
+++ src/view/LoginPage/LoginWindow.java
@@ -0,0 +1,54 @@
+package view.LoginPage;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class LoginWindow extends JFrame implements ActionListener {
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+
+    public LoginWindow() {
+        setTitle("Login Window");
+        setSize(500, 350);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 2));
+
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameField = new JTextField();
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordField = new JPasswordField();
+        JButton loginButton = new JButton("Login");
+
+        loginButton.addActionListener(this);
+
+        panel.add(usernameLabel);
+        panel.add(usernameField);
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+        panel.add(new JLabel()); // Placeholder
+        panel.add(loginButton);
+
+        add(panel);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String username = usernameField.getText();
+        String password = String.valueOf(passwordField.getPassword());
+
+        // For simplicity, let's just check if the username is "user" and password is "password"
+        if (username.equals("user") && password.equals("password")) {
+            JOptionPane.showMessageDialog(this, "Login Successful!");
+            // You can add code here to open a new window or perform further actions after successful login
+        } else {
+            JOptionPane.showMessageDialog(this, "Invalid username or password. Please try again.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new LoginWindow());
+    }
+}
