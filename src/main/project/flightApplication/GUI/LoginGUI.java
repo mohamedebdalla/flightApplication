c1e3c38fbea2d87b13b8aa3c9c7a884273038946
anterakotoarison <113263127+anterakotoarison@users.noreply.github.com>
2023-12-01 16:45:07 -0700

    admin

--- src/main/project/flightApplication/GUI/LoginGUI.java
+++ src/main/project/flightApplication/GUI/LoginGUI.java
@@ -14,12 +14,14 @@ public class LoginGUI extends JDialog{
     LoginController loginController;
     private JTextField usernameField;
     private JPasswordField passwordField;
+    private String userType;
     DBcore dbcore = DBcore.getInstance();
     private Admin admin = new Admin();
 
-    public LoginGUI(Frame parent, MainGUI mainGUI) {
+    public LoginGUI(Frame parent, MainGUI mainGUI, String userType) {
         super(parent, "Login", true);
         this.gui = mainGUI;
+        this.userType = userType;
 
         // Create components
         JLabel usernameLabel = new JLabel("Username:");
@@ -32,10 +34,10 @@ public class LoginGUI extends JDialog{
 
         loginButton.addActionListener(e ->{
             System.out.println("please work");
-            if(isValidUser(usernameField.getText(), passwordField.getText())){
+            if(isValidUser(usernameField.getText(), passwordField.getText(), userType)){
                 System.out.println("valid");
                 JOptionPane.showMessageDialog(null, "Login Successful!");
-                gui.displayPage(usernameField.getText());
+                displayPage(userType);
                 this.dispose();
 
             }
@@ -65,14 +67,25 @@ public class LoginGUI extends JDialog{
         setVisible(true);
     }
 
-    public boolean isValidUser(String username, String password) {
+    public void displayPage(String userType){
+        if(userType == "registered user"){
+            gui.displayPage(usernameField.getText());
+        }
+        else if(userType == "admin"){
+            AdminPage adminPage = new AdminPage();
+            System.out.println("admin page display");
+        }
+
+    }
+
+    public boolean isValidUser(String username, String password, String userType) {
         try{
             String query = "SELECT * FROM users WHERE username = ? AND password = ? AND userType = ?";
 
             try(PreparedStatement preparedStatement = dbcore.getConnection().prepareStatement(query)){
                 preparedStatement.setString(1, username);
                 preparedStatement.setString(2, password);
-                preparedStatement.setString(3, "registered user");
+                preparedStatement.setString(3, userType);
 
                 ResultSet resultSet = preparedStatement.executeQuery();
                 return resultSet.next();
